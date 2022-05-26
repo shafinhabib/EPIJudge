dacb12b5a44df3116f85f761d525e93de60b49b7
tibjo3002 <86914742+shafinhabib@users.noreply.github.com>
2022-05-26 20:00:18 +0300

    adv_array_5.4

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -3,8 +3,23 @@ from typing import List
 from test_framework import generic_test
 
 
-def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
+def can_reach_end(arr: List[int]) -> bool:
+    idx, idx2 = 0, 0
+    m_score = idx + arr[idx]
+    p_score = 0
+
+    while m_score < len(arr)-1:
+        idx2, p_score = 0, 0
+        for i in range(1, arr[idx]+1):
+            if m_score <= arr[idx+i]+i+idx:
+                if p_score <= arr[idx+i]+i+idx:
+                    p_score = arr[idx+i]+i+idx
+                    idx2 = idx+i
+        if idx2 == 0:
+            return False
+        else:
+            idx = idx2
+            m_score = p_score
     return True
 
 
