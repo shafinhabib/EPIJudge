a42e1c7a92359e0259b3a24dff581298e99cae1e
tibjo3002 <86914742+shafinhabib@users.noreply.github.com>
2022-06-10 03:06:08 +0300

    Spreadsheet column conversion

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,9 +1,15 @@
 from test_framework import generic_test
+import string
 
+def ss_decode_col_id(t: str) -> int:
+    s = list(string.ascii_uppercase)
+    res = 0
+    dic = {s[i-1]:i for i in range(1,27)}
 
-def ss_decode_col_id(col: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    for i in reversed(range(len(t))):
+        res += dic[t[~i]]*26**i
+
+    return res
 
 
 if __name__ == '__main__':
