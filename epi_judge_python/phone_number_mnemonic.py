f07cad5f410b8c32e188a07652fe6f6ce2739276
tibjo3002 <86914742+shafinhabib@users.noreply.github.com>
2022-06-16 23:18:02 +0300

    phone_mnemonic

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -4,8 +4,20 @@ from test_framework import generic_test, test_utils
 
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    dic = {1:'1', 2:('A','B','C'), 3:('D','E','F'), 4:('G','H','I'),\
+    5:('J','K','L'), 6:('M','N','O'), 7:('P','Q','R','S'), 8:('T','U','V'),\
+    9:('W','X','Y','Z'), 0:'0'}
+
+    res = []
+    def mnemonics(t,i):
+        if i < 0:
+            res.append(t)
+            return
+        for letters in dic[int(phone_number[i])]:
+            mnemonics(letters+t, i-1)
+
+    mnemonics('', len(phone_number)-1)
+    return res
 
 
 if __name__ == '__main__':
